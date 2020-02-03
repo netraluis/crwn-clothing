b84904258ca6296e3c69491e4f8646eb5397edcf
funzza <netraluis@gmail.com>
2020-02-03 18:47:53 +0100

    shopPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 import HomePage from './pages/homepages/homepage.component';
+
+
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component = {HomePage} />
+        <Route path='/shop' component = {ShopPage}/>
+      </Switch>
     </div>
   );
 }
