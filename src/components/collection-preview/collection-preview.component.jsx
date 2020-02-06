575150ca30a7ec86b5d6381d7cc86da14496f44a
netraluis.castells@corusconsulting.com <netraluis.castells@corusconsulting.com>
2020-02-06 10:28:33 +0100

    css Add To Cart

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import CollectionItem from './../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
+
+
 const CollectionPreview = ({title,items})=>(
     <div>
         <div className = 'collection-preview'>
